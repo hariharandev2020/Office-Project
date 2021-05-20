7a2ca868123d87ac5d3afd08a5c60c2fe3b056ec
hariharandev2020 <hariharan29220gmail.com>
2021-05-20 12:54:35 +0530

    Make some  changes on Table

--- HariHaran/Billing-Software/JS/billing.js
+++ HariHaran/Billing-Software/JS/billing.js
@@ -7,16 +7,41 @@ function add(){
     let customerName = document.querySelector("#cname").value;
     let productName  = document.querySelector("#pname").value;
     let productCode  = document.querySelector("#pcode").value;
-    let pprice       = document.querySelector("#ppu").value;
+    let unitPrice    = document.querySelector("#ppu").value;
+    let quantity     = document.querySelector("#qty").value;
     
 
      document.querySelector("#cusid").innerHTML   =  "Customer Id :" + " " + cid;
      document.querySelector("#cusname").innerHTML =   "Customer Name :" + " " + customerName;
      document.querySelector("#content-1-header-1").style.backgroundColor = "white";
 
-     let text = document.createTextNode(productName);
+     let text = document.createTextNode(productCode);
      var li           = document.createElement("li");
      li.appendChild(text);
      document.querySelector(".row-1").appendChild(li);
- 
+    
+     // ...............................................................
+     
+     let text1 = document.createTextNode(productName);
+     var li           = document.createElement("li");
+     li.appendChild(text1);
+     document.querySelector(".row-2").appendChild(li);
+
+     //.................................................................
+
+     let text2 = document.createTextNode(quantity);
+     var li           = document.createElement("li");
+     li.appendChild(text2);
+     document.querySelector(".row-3").appendChild(li);
+
+     //...................................................................
+
+
+     let text3 = document.createTextNode(quantity * unitPrice);
+     var li           = document.createElement("li");
+     li.appendChild(text3);
+     document.querySelector(".row-4").appendChild(li);
+
+     //...................................................................
+
 }
